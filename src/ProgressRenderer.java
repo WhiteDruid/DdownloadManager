d1a0fc6139609ea487bfc8660367a663648ff357
WhiteDruid <tahah2000@gmail.com>
2017-03-09 17:59:48 +0330

    update

--- src/ProgressRenderer.java
+++ src/ProgressRenderer.java
@@ -1,4 +1,28 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
 
-public class ProgressRenderer {
+public class ProgressRenderer extends JProgressBar implements TableCellRenderer {
+	
+		public ProgressRenderer(int min , int max) {
+			super(min , max);
+		}
+		
+		/* Returns this JProgressBar as the renderer
+	     for the given table cell. */
+		public Component  getTableRenderComponent( JTable table , Object value , boolean isSelected ,
+				 boolean hasFocus , int row , int colum ) {
+		       // Set JProgressBar's percent complete value.
+			         setValue((int) ((Float) value).floatValue());
+					return this;
+			
+		}
 
-}
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			// TODO Auto-generated method stub
+			return null;
+		}		
+		
+	}
